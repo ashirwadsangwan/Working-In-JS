8937865cd18e746f730bc071ea05571185d77843
Ashirwad Sangwan <ashirwadsangwan@gmail.com>
2019-06-25 01:57:26 +0530

    First commit

---
+++ Basics/dataTypes.js
@@ -0,0 +1,13 @@
+// String datatypes
+
+console.log("Hello, Javascript!!!")
+
+// Number datatypes: Both integers and floats are just number types in JS.
+
+console.log(451)
+console.log(2.31)
+
+// Boolean datatypes
+
+console.log(true)
+console.log(false)
