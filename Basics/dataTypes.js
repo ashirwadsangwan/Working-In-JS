5d006c26cdd9daf1d0eee241ea5932d17904e020
Ashirwad Sangwan <ashirwadsangwan@gmail.com>
2019-06-25 15:58:03 +0530

    datatypes done!!!

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -11,3 +11,31 @@ console.log(2.31)
 
 console.log(true)
 console.log(false)
+
+// nullType
+var emptyStuff = null
+console.log(emptyStuff)
+
+// undefined data types
+
+var success;
+console.log(success)
+
+// Arrays
+var fruits  = ['apple', 'orange', 'mango']
+var nums = [1, 2, 3]
+console.log(fruits)
+
+// objects
+
+var johnny = {
+  'name': 'johnny bravo',
+  'age': 'pretty young',
+  'mood': 'sexy',
+  'profession': 'impressing chicks'
+}
+console.log(johnny.name)
+console.log(johnny.mood)
+console.log(johnny.age)
+console.log(johnny.place)
+
